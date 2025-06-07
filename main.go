78dd78558ea41c9bdf094bced9e39d05c01e65e6
unknown <ovieighosuakpo@gmail.com>
2025-06-07 17:27:27 +0100

    added xss protection

--- main.go
+++ main.go
@@ -23,9 +23,27 @@ func main() {
 	if err != nil {
 		fmt.Println("Error loading .env file")
 	}
+
+	PORT := os.Getenv("PORT")
+
+	expectedHost := "localhost:" + PORT
 	router := gin.New()
 	router.Use(gin.Recovery())
 	router.Use(limit.MaxAllowed(200))
+	router.Use(func(c *gin.Context) {
+		if c.Request.Host != expectedHost {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid host header"})
+			return
+		}
+		c.Header("X-Frame-Options", "DENY")
+		c.Header("Content-Security-Policy", "default-src 'self'; connect-src *; font-src *; script-src-elem * 'unsafe-inline'; img-src * data:; style-src * 'unsafe-inline';")
+		c.Header("X-XSS-Protection", "1; mode=block")
+		c.Header("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
+		c.Header("Referrer-Policy", "strict-origin")
+		c.Header("X-Content-Type-Options", "nosniff")
+		c.Header("Permissions-Policy", "geolocation=(),midi=(),sync-xhr=(),microphone=(),camera=(),magnetometer=(),gyroscope=(),fullscreen=(self),payment=()")
+		c.Next()
+	})
 
 	//initialise DB
 	config.ConnectDB()
@@ -44,7 +62,7 @@ func main() {
 	router.Use(cors.New(corsConfig))
 
 	//startup server
-	PORT := os.Getenv("PORT")
+
 	s := &http.Server{
 		Addr:           ":" + PORT,
 		Handler:        router,
